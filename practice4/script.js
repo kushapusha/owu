6d9d6c55aa707b0a010a6d555db128f4c76cfcde
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-08-31 19:08:25 +0300

    Add files via upload

---
+++ practice4/script.js
@@ -0,0 +1,76 @@
+// function first(){
+//     let arr1 = Object.values(arguments);
+//     console.log(arr1);
+// }
+// first(53,4,3,35,3,45,35,35);
+
+
+// function second(aaa){
+//     let arr=[];
+//     while(arr.length < aaa){
+//     let randomNumber = parseInt(Math.random()*99);
+//     arr.push(randomNumber);
+// }
+//     let big = arr[0];
+//     for(let num of arr) {
+//         if(big <num ){
+//             big = num
+//         }
+//     }
+//     console.log(big);
+//     first(arr);
+// }
+// second(10);
+
+
+
+
+// function fourth(a){
+//     let arr1 = [];
+//     let arr2 = [];
+//     let arr3 = [];
+//     while(arr1.length < a && arr2.length < a){
+//         let randomNumber1 = parseInt(Math.random()*9);
+//         let randomNumber2 = parseInt(Math.random()*9);
+//         arr1.push(randomNumber1);
+//         arr2.push(randomNumber2);
+//     }
+//     console.log(arr1);
+//     console.log(arr2);
+
+    // for (let item of arr2){
+    //     arr1.push(item)
+    // }
+    // console.log(arr1);
+
+//     for (i=0; i<a; i++){
+//         arr3[i] = arr1[i] + arr2[i];
+//     }
+//     console.log(arr3);
+// }
+// fourth(4);
+
+
+
+
+// function six(a){
+//     let arr=[];
+//         while(arr.length < a){
+//         let randomNumber = parseInt(Math.random()*5);
+//         arr.push(randomNumber);
+//     }
+//     console.log(arr);
+//     let arr1 = [];
+//     for (i=0; i<arr.length; i++){
+//         if (arr[i] !== 0) {
+//             arr1.push(arr[i])
+//         }
+//     }
+//     for (i=0; i<arr.length; i++){
+//         if (arr[i] === 0) {
+//             arr1.push(arr[i])
+//         }
+//     }
+//     console.log(arr1);
+// }
+// six(2);
