50bcba9e43c675cbcccfd3ecb595fefcf91b817f
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-09-07 18:56:35 +0300

    new html

--- practice7/script.js
+++ practice7/script.js
@@ -1,3 +1,8 @@
+
+//Factorial
+
+
+
 // function fakt(num, callback){
 //     if (num != 1){
 //         return (num*callback(num-1, callback));
@@ -8,28 +13,35 @@
 
 
 
-
 // function fakt(num, callback){
 //     return(num != 1)?(num*callback(num-1, callback)):1
 // };
 // console.log(fakt(5, fakt));
 
 
+// Fibonachi
 
-// let arr = [1,2];
+
+// function fibonacci(max) {
+//     let result = [1, 2];
+
+//     for (let i = 1; result[i] < max; i++) {
+//         result.push(result[i] + result[i - 1]);
+//     }
+
+//     console.log(result);
+
+// }
+
+// fibonacci(20);
 
 
-function fibonacci(max) {
-    let result = [1, 2];
-
-    for (let i = 1; result[i] < max; i++) {
-        result.push(result[i] + result[i - 1]);
+function factorial(num){
+    let res = 1;
+    for (i = 1; i <= num; i++){
+        res *= i;
     }
-
-    console.log(result);
-
+    return res
 }
 
-fibonacci(20);
-
-
+factorial(5);
