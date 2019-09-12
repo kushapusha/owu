cb128c23d86ca66bb5f34a969d36f50303bf9b92
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-09-12 13:46:05 +0300

    array in array, object in array

--- lecture7(hw)/script.js
+++ lecture7(hw)/script.js
@@ -1,3 +1,26 @@
 let arr = [2,4,{name: 'Ginger', age: 19, cat: true},6,[34,6,2],4,7];
 
 
+function OpenArray(someArray){
+    if (!Array.isArray(someArray)) {
+        return;
+    }
+
+    someArray.forEach(value => {
+
+        if (Array.isArray(value)){
+            OpenArray(value);
+            return;
+        }
+
+        if (typeof value === 'object' && value !== null){
+            OpenArray(Object.values(value));
+            return;
+        }
+
+        console.log(value);
+
+    })
+}
+
+OpenArray(arr);
