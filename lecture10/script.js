14c96440997b40fd616cb7208b2731ddb3672bcd
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-09-13 15:35:00 +0300

    Update script.js

--- lecture10/script.js
+++ lecture10/script.js
@@ -55,7 +55,59 @@ function Dinner(){
 )}
 
 
+Awake()
+    .then(value => {
+        console.log(value);
+        return new Promise((resolve, reject) => {
+            setInterval(()=>{
+                resolve('Прийняла душ. Яка класна водичка')
+            }, 1000)
+        })
+    })
 
+    .then(value => {
+        console.log(value);
+        return new Promise((resolve, reject) => {
+            setInterval(()=>{
+                resolve('Поїла. Класний сніданок')
+            }, 2000)
+        })
+    })
 
+    .then(value => {
+        console.log(value);
+        return new Promise((resolve, reject) => {
+            setInterval(()=>{
+                resolve('Намалювалась. Тепер це табло можна виводити в люди')
+            }, 1000)
+        })
+    })
+
+    .then(value => {
+        console.log(value);
+        return new Promise((resolve, reject) => {
+            setInterval(()=>{
+                resolve('На роботі. Хочу додому!!!')
+            }, 2000)
+        })
+    })
+
+    .then(value => {
+        console.log(value);
+        return new Promise((resolve, reject) => {
+            setInterval(()=>{
+                resolve('О так, я дома. Привіт, пухнаста дупка:)')
+            }, 500)
+        })
+    })
+
+    .then(value => {
+        console.log(value);
+        return new Promise((resolve, reject) => {
+            setInterval(()=>{
+                resolve('Повечеряла. Умм вкусняшки.')
+            }, 1000)
+        })
+    })
 
 
