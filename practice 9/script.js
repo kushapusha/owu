5335f8ab6822f1c4797c99be4d1186ac899fbcbc
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-09-14 14:13:56 +0300

    Update script.js

--- practice 9/script.js
+++ practice 9/script.js
@@ -25,11 +25,7 @@ arr = [12, 5, {name: 'lala', id: 555}, {name: 'dlcd', age: 8}, 6, 'egeg', {age: 
 console.log(arr);
 
 Array.prototype.justID = function() {
-    for (let i = 0; i < this.length; i++) {
-
-    this[i].id ? console.log(this[i]) : 0;  
-
-    }
+    return this.filter(value => value.hasOwnProperty('id'))
 }
 
-arr.justID();
+console.log(arr.justID());
