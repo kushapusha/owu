89a74e3bf283dcc5857ea10f9ced40ae59ba61db
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-09-15 01:26:37 +0300

    Update script.js

--- consultation/script.js
+++ consultation/script.js
@@ -1,11 +1,37 @@
-let Arr = [1, 3, 4, 1, 7, 4, 4, 7, 9, 3, 2, 6, 4];
+// let Arr = [1, 3, 4, 1, 7, 4, 4, 7, 9, 3, 2, 6, 4];
 
-function MinMax(arr, el){
-    let Min = arr.indexOf(el);
-    let Max = arr.lastIndexOf(el);
+// function MinMax(arr, el){
+//     let Min = arr.indexOf(el);
+//     let Max = arr.lastIndexOf(el);
 
-    console.log('MinIndex = ', Min);
-    console.log('MaxIndex = ', Max);
+//     console.log('MinIndex = ', Min);
+//     console.log('MaxIndex = ', Max);
+// }
+
+// MinMax(Arr, 1);
+
+
+
+
+
+function Party(n, m){
+    let arr = Array.from(Array(n), () => new Array(m));
+    let arr1 = Array.from(Array(m), () => new Array(n));  
+
+
+    for (let i = 0; i < n; i++){
+        for (let j = 0; j < m; j++){
+            arr[i][j] = parseInt(Math.random()*(n*m));
+        }
+    }   
+    console.log(arr);
+
 }
 
-MinMax(Arr, 0);
+Party(3, 5);
+
+
+
+
+
+
