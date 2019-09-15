63d9b0a425a49bd27b01ee544b5a6e5c11e4a867
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-09-15 22:02:35 +0300

    Update script.js

--- consultation/script.js
+++ consultation/script.js
@@ -1,34 +1,56 @@
-// let Arr = [1, 3, 4, 1, 7, 4, 4, 7, 9, 3, 2, 6, 4];
 
-// function MinMax(arr, el){
-//     let Min = arr.indexOf(el);
-//     let Max = arr.lastIndexOf(el);
-
-//     console.log('MinIndex = ', Min);
-//     console.log('MaxIndex = ', Max);
-// }
-
-// MinMax(Arr, 1);
-
-
-
-
-
-function Party(n, m){
-    let arr = Array.from(Array(n), () => new Array(m));
-    let arr1 = Array.from(Array(m), () => new Array(n));  
-
-
+function MinMax(n, num){
+    let arr = [];
     for (let i = 0; i < n; i++){
-        for (let j = 0; j < m; j++){
-            arr[i][j] = parseInt(Math.random()*(n*m));
-        }
-    }   
+        let el = parseInt(Math.random()*9);
+        arr.push(el);
+    }
+    arr.sort((el1, el2) => {
+        return el1 - el2
+    })
+
+    let Min = arr.indexOf(num);
+    let Max = arr.lastIndexOf(num);
+
     console.log(arr);
+    
+    console.log(`MinIndex = ${Min}`);
+    console.log(`MaxIndex = ${Max}`);
 
 }
+MinMax(10, 5);
 
-Party(3, 5);
+
+
+
+
+
+
+
+
+// function Party(n, m){
+//     let arr1 = Array.from(Array(n), () => new Array(m));
+//     let arr2 = Array.from(Array(n), () => new Array(m));
+//     let stay = 0;
+      
+//     for (let i = 0; i < n; i++){
+//         for (let j = 0; j < m; j++){
+//             arr1[i][j] = i*m + j + 1;
+//             arr2[i][j] = j*n + i + 1;
+
+//             if (arr1[i][j] === arr2[i][j]){
+//                 stay += 1;
+//             }
+//         }  
+//     } 
+
+//  console.log(arr1);
+//  console.log(arr2);
+//  console.log(`На своїх місцях залишилось ${stay} батьків`);
+
+// }
+
+// Party(3, 5);
 
 
 
